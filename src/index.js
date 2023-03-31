fb652fac481438c116f8c69d45fb8ace26c6fc65
lauchrens <104091795+lauchrens@users.noreply.github.com>
2023-04-01 00:56:50 +0200

    pushed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDom from "react-dom"
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Nigger () {
+  return (
+    <div className="home-content">
+      <h4>I hate NIGGERS</h4>
+    </div>
+  )
+}
+
+function Profile () {
+  return (
+    <div className="nav-profile">
+      <a href='https://youtube.com' to='/'>
+         <img src='https://cdn.discordapp.com/attachments/979283646571749396/1090292278607753256/br.png' alt='' width="50"/>
+      </a>
+    </div>
+  )
+}
+
+function Navbar () {
+  return (
+    <div className="nav">
+      <nav className='navG'>
+        <ul>
+          <a className='home-nav-css' href='home' to='/'>Home</a>
+          <a href='about' to='/'>About</a>
+          <a href='contact' to='/'>Contact</a>
+          <Profile/>
+        </ul>
+        
+      </nav>
+    </div>
+  )
+}
+
+function Cum() {
+  return (
+    <>
+    <Navbar/>
+    <Nigger/>
+    </>
+  )
+}
+
+ReactDom.render(<Cum/>,document.getElementById('root'))
