93992a67fee70c01540b2e2189466d59c19fcd3b
Kiwi <84627732+Kiwious@users.noreply.github.com>
2023-04-01 10:14:29 +0200

    remove yt href

--- src/Profile.js
+++ src/Profile.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Profile(props) {
     return (
         <div className="nav-profile">
-          <a href='https://youtube.com' to='/'>
-             <img src={props.img_link} alt='' width="50"/>
+          <a href='' to='/'>
+            <img src={props.img_link} alt='' width="50" />
           </a>
         </div>
       )
