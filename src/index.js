380169b4bb56f1562db13f931312d745e5d148c3
Kiwi <84627732+Kiwious@users.noreply.github.com>
2023-04-01 09:30:31 +0200

    add react 17 render

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App'
 import './styles.css'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
   <React.StrictMode>
     <App/>
   </React.StrictMode>,
