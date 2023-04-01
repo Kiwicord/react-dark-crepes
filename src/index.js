d5589d768b9d559936d3ceef81e1f206937e24e6
Kiwi <84627732+Kiwious@users.noreply.github.com>
2023-04-01 10:14:11 +0200

    Move css files to a seperate folder in src

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 
 import App from './App'
-import './styles.css'
+import './styles/styles.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
