730a02953fedc4cf713ca71a520684b6aa00691d
lauchrens <104091795+lauchrens@users.noreply.github.com>
2023-04-01 23:15:06 +0200

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Profile(props) {
     return (
         <div className="nav-profile">
-          <a href='' to='/'>
+          <a href='profile' to='/'>
             <img src={props.img_link} alt='' width="50" />
           </a>
         </div>
