ecb0b2029b2b3c35cbae23ff4108aa7f392d0387
Kiwi <84627732+Kiwious@users.noreply.github.com>
2023-04-02 13:36:14 +0200

    ProfileView added

--- src/Profile.js
+++ src/Profile.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ProfileView from './ProfileView'
 
 export default function Profile(props) {
+    const [onHoverContent, setHoverContent] = useState(false)
     return (
-        <div className="nav-profile">
-          <a href='' to='/'>
-            <img src={props.img_link} alt='' width="50" />
+        <div className="nav-profile" style={props.disabled ? {pointerEvents: "none", opacity: "0.4"} : {}}>
+          <a href='https://youtube.com/' onMouseEnter={() => setHoverContent(true)} onMouseLeave={() => setHoverContent(false)}>
+            {onHoverContent === true && <ProfileView profileName='Kiwious' friendsCount='1'/>}
+            <img className='profile-img' src={props.img_link} alt='' width="50" />
           </a>
         </div>
       )
--- src/ProfileView.js
+++ src/ProfileView.js
@@ -1,11 +1,10 @@
 import React from "react";
-import './styles/profileview.css'
 
 export default function ProfileView(props) {
     return (
         <div className="profile-view">
-            <h1>{props.profileName}</h1>
-            <img src={props.profileImage} alt="profilepfp" />
+            <h2 className="profile-name">{props.profileName}</h2>
+            <p>Friends: {props.friendsCount}</p>
         </div>
     )
 }
