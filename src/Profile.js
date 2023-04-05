21ab95ff60c7f30304d6a74ae37ffc5cdd8aede1
lauchrens <104091795+lauchrens@users.noreply.github.com>
2023-04-05 21:37:03 +0200

    mongodb try #1

--- src/Profile.js
+++ src/Profile.js
@@ -5,7 +5,7 @@ export default function Profile(props) {
     const [onHoverContent, setHoverContent] = useState(false)
     return (
         <div className="nav-profile" style={props.disabled ? {pointerEvents: "none", opacity: "0.4"} : {}}>
-          <a href='https://youtube.com/' onMouseEnter={() => setHoverContent(true)} onMouseLeave={() => setHoverContent(false)}>
+          <a href='profile' onMouseEnter={() => setHoverContent(true)} onMouseLeave={() => setHoverContent(false)}>
             {onHoverContent === true && <ProfileView profileName='Kiwious' friendsCount='1'/>}
             <img className='profile-img' src={props.img_link} alt='' width="50" />
           </a>
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './styles.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
+
 root.render(
   <React.StrictMode>
     <App/>
