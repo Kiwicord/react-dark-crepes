f82c784267aee5d509561b23cde1cb49ec302f31
lauchrens <104091795+lauchrens@users.noreply.github.com>
2023-04-05 23:37:03 +0200

    Update MainContent.js

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function MainContent() {
+
+    const [value, setValue] = useState('Kiwi Stinkt SEHR dolle...');
+
+    function KillYourSelf() {
+      setValue('KAPPA');
+    }
+
     return (
         <div className="home-content">
           <h4>I hate NIGGERS</h4>
+          <button onClick={KillYourSelf}>Free Candy</button>
+          <p>{value}</p>
         </div>
       ) 
 }
