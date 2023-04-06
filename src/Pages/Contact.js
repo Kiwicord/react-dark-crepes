3f06f0a97464d0f8f5d913f86ca38b1e08276e51
lauchrens <104091795+lauchrens@users.noreply.github.com>
2023-04-06 22:58:56 +0200

    Update Contact.js

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
                 - Besuche unser Discord-Server und sende uns eine Direktnachricht oder eine Nachricht im öffentlichen Chat. Unsere Teammitglieder und Community-Moderatoren helfen gerne bei Fragen oder Problemen weiter.
             </div>
             <div id="trese">
-                - Du kannst uns auch eine E-Mail an <a href='contact@kiwicord.com' target="_blank">contact@kiwicord.com</a> senden. Wir bemühen uns, alle E-Mails innerhalb von 24 Stunden zu beantworten.
+                - Du kannst uns auch eine E-Mail an <a href='contact@kiwicord.de' target="_blank">contact@kiwicord.de</a> senden. Wir bemühen uns, alle E-Mails innerhalb von 24 Stunden zu beantworten.
             </div>
             <div id="quatro">
                 - Wenn du uns auf anderen Social-Media-Plattformen folgen möchtest, findest du uns unter @kiwicord auf Twitter und Instagram. Dort posten wir regelmäßig Updates und Neuigkeiten zu Kiwicord und unseren Projekten.
