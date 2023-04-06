d848a3c707802fcac0ad936671e9d184c78fd1bd
lauchrens <104091795+lauchrens@users.noreply.github.com>
2023-04-06 22:57:07 +0200

    contact

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -2,7 +2,23 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <h1>Contact page</h1>
+        <div className="contact-content">
+            <div id="uno">
+                Wir bei Kiwicord freuen uns immer über Feedback und Fragen von unseren Mitgliedern und der Discord-Community. Wenn du eine Frage hast oder Hilfe benötigst, zögere bitte nicht, uns zu kontaktieren. Hier sind einige Möglichkeiten, wie du uns erreichen kannst:
+            </div>
+            <div id="doux">
+                - Besuche unser Discord-Server und sende uns eine Direktnachricht oder eine Nachricht im öffentlichen Chat. Unsere Teammitglieder und Community-Moderatoren helfen gerne bei Fragen oder Problemen weiter.
+            </div>
+            <div id="trese">
+                - Du kannst uns auch eine E-Mail an <a href='contact@kiwicord.com' target="_blank">contact@kiwicord.com</a> senden. Wir bemühen uns, alle E-Mails innerhalb von 24 Stunden zu beantworten.
+            </div>
+            <div id="quatro">
+                - Wenn du uns auf anderen Social-Media-Plattformen folgen möchtest, findest du uns unter @kiwicord auf Twitter und Instagram. Dort posten wir regelmäßig Updates und Neuigkeiten zu Kiwicord und unseren Projekten.
+            </div>
+            <div id="longo">
+                Vielen Dank für dein Interesse an Kiwicord. Wir freuen uns darauf, von dir zu hören!
+            </div>
+        </div>
     )
 }
 
