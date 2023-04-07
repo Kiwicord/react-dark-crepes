2973c1c5da344303bcbbfe7bf8f06bccd1698394
Kiwi <84627732+Kiwious@users.noreply.github.com>
2023-04-07 10:12:52 +0200

    Profile asssets name change

---
+++ src/ProfileImg.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import ProfileView from './ProfileView'
+
+const ProfileImg = (props) => {
+    const [onHoverContent, setHoverContent] = useState(false)
+    return (
+        <div className="nav-profile" style={props.disabled ? {pointerEvents: "none", opacity: "0.4"} : {}}>
+          <a href='profile' onMouseEnter={() => setHoverContent(true)} onMouseLeave={() => setHoverContent(false)}>
+            {onHoverContent === true && <ProfileView profileName='Kiwious' friendsCount='1'/>}
+            <img className='profile-img' src={props.img_link} alt='' width="50" />
+          </a>
+        </div>
+      )
+}
+
+export default ProfileImg;
