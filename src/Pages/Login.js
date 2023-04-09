79a7d0bb90191eae04f27b3dd70305db0ab85b00
lauchrens <104091795+lauchrens@users.noreply.github.com>
2023-04-10 01:04:47 +0200

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -7,8 +7,8 @@ const Login = () => {
     // User Login info
     const database = [
       {
-        username: "admin",
-        password: "nagger"
+        username: "*",
+        password: "*"
       }
     ];
 
